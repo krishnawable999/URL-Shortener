2c01cdaba26b4508ffa0e12977967d41aa0e7cd5
Krishna <krishnawable999@gmail.com>
2024-04-12 16:14:39 +0530

    working code

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/test", async(req,res)=>{
 });
 app.use(coockieParcer());
 app.use(checkforAuthentication);
-app.use("/url",restrictTo("[NORMAL]"),urlRoute);
+app.use("/url",restrictTo(["NORMAL"],["ADMIN"]),urlRoute);
 app.use("/user",userRoute);
 app.use("/",staticRoute);
 
